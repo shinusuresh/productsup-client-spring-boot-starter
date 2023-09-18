bf38e24f01f16adcdd5dc7f3e10846fb4c506021
Shinu Suresh <mailtoshinu@gmail.com>
2023-09-18 21:04:48 +0100

    #43: Adding support to delete products

--- src/test/java/io/github/shinusuresh/productsup/client/ProductsPlatformApiClientTest.java
+++ src/test/java/io/github/shinusuresh/productsup/client/ProductsPlatformApiClientTest.java
@@ -1,14 +1,14 @@
 package io.github.shinusuresh.productsup.client;
 
-import org.junit.jupiter.api.Test;
-import org.mockserver.client.MockServerClient;
-import org.mockserver.model.MediaType;
-import org.mockserver.model.Parameter;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
+
+import org.junit.jupiter.api.Test;
+import org.mockserver.client.MockServerClient;
+import org.mockserver.model.MediaType;
+import org.mockserver.model.Parameter;
 
 class ProductsPlatformApiClientTest extends BasePlatformApiClient {
 
@@ -18,39 +18,60 @@ class ProductsPlatformApiClientTest extends BasePlatformApiClient {
 
     @Test
     void testProducts() {
-        try (var mockClient = mockClient()) {
-            mockClient.when(request()
-                            .withMethod("GET")
-                            .withPath("/site/{id}/stage/{stageName}")
-                            .withPathParameters(
-                                    new Parameter("id", "[0-9]+"),
-                                    new Parameter("stageName", "[a-z]+")
-                            )
-                    )
-                    .respond(response()
-                            .withContentType(MediaType.APPLICATION_JSON)
-                            .withBody(
-                                    """
+        mockClient().when(request()
+                .withMethod("GET")
+                .withPath("/site/{id}/stage/{stageName}")
+                .withPathParameters(
+                    new Parameter("id", "[0-9]+"),
+                    new Parameter("stageName", "[a-z]+")
+                )
+            )
+            .respond(response()
+                .withContentType(MediaType.APPLICATION_JSON)
+                .withBody(
+                    """
 
-                                                {
-                                                 "success": true,
-                                                 "products": [{
-                                                     "id": "123",
-                                                     "gtin": "42"
-                                                 }]
-                                             }
-                                            """
-                            ));
-            var productsList = platformApiClient().getProducts(123, "import", null, null);
-            var products = productsList.products()
-                    .stream()
-                    .findFirst()
-                    .get();
-            assertThat(products)
-                    .containsExactly(
-                            entry("id", "123"),
-                            entry("gtin", "42")
-                    );
-        }
+                            {
+                             "success": true,
+                             "products": [{
+                                 "id": "123",
+                                 "gtin": "42"
+                             }]
+                         }
+                        """
+                ));
+        var productsList = platformApiClient().getProducts(123, "import", null, null);
+        var products = productsList.products()
+            .stream()
+            .findFirst()
+            .get();
+        assertThat(products)
+            .containsExactly(
+                entry("id", "123"),
+                entry("gtin", "42")
+            );
+    }
+
+    @Test
+    void testDeleteAllProducts() {
+        mockClient()
+            .when(request()
+                .withMethod("DELETE")
+                .withPath("/sites/{id}/products")
+                .withPathParameters(
+                    new Parameter("id", "[0-9a-zA-Z]+")
+                )
+            )
+            .respond(response()
+                .withContentType(MediaType.APPLICATION_JSON)
+                .withBody(
+                    """
+                            {
+                             "success": true                                  
+                         }
+                        """
+                ));
+        var deleteProductsResponse = platformApiClient().deleteAllProducts("123");
+        assertThat(deleteProductsResponse.success()).isTrue();
     }
 }
